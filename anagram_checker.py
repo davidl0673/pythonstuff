24993b3a9c22bb2ca338905988ca000538e9161e
davidl0673 <48873196+davidl0673@users.noreply.github.com>
2019-04-02 18:00:15 -0700

    Add files via upload

---
+++ anagram_checker.py
@@ -0,0 +1,11 @@
+
+word1 = input('pick a word ')
+sorted(word1) = word1.lower()
+
+word2 = input('pick another word')
+sorted(word_list)
+word_string2 = 'anagram'
+word_list2 = list(word_string2)
+sorted(word_list2)
+
+word_list == sorted(word_list2)
