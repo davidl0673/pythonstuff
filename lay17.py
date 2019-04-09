05fc7fabbb7140590cf252859eb320f9f7e9c06c
davidl0673 <48873196+davidl0673@users.noreply.github.com>
2019-04-08 18:35:26 -0700

    Delete lay17.py

--- lay17.py
+++
@@ -1,8 +0,0 @@
-def Palindrome_check(word):
-        word = input( 'pick a name ').lower()
-        if word == word[::-1]:
-            print('yes')
-        else:
-            print('no')
-
-Palindrome_check('david')
